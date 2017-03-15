bc0070bab0eb46ecc634618553bf35cf63c290f2
Chad Killingsworth <chad.killingsworth@banno.com>
2017-03-14 19:18:50 -0500

    Make sure only one bail config option is listed

--- wdio.conf.js
+++ wdio.conf.js
@@ -16,8 +16,6 @@ exports.config = {
     exclude: [
         // 'path/to/excluded/files'
     ],
-
-    bail: 1,
     //
     // ============
     // Capabilities
@@ -67,7 +65,7 @@ exports.config = {
     //
     // If you only want to run your tests until a specific amount of tests have failed use
     // bail (default is 0 - don't bail, run all tests).
-    bail: 0,
+    bail: 1,
     //
     // Saves a screenshot to a given path if a command fails.
     screenshotPath: './errorShots/',
